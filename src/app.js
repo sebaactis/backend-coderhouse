2df45b308e7d18c77c043a7ce17935c51a78f682
sebaactis <sebaactis@gmail.com>
2023-03-23 09:25:31 -0300

    correccion async

--- src/app.js
+++ src/app.js
@@ -8,42 +8,33 @@ const manager = new ProductManager;
 
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/products', (req, res) => {
+app.get('/products', async (req, res) => {
 
     const limit = req.query.limit;
 
-    const main = async () => {
+    if (limit < 1) {
+        res.send(await manager.getProducts());
+    } else {
+        const products = await manager.getProducts();
 
-        if (limit < 1) {
-            res.send(await manager.getProducts());
-        } else {
-            const products = await manager.getProducts();
-
-            res.send(products.slice(0, limit));
-        }
+        res.send(products.slice(0, limit));
     }
-    main();
+}
+)
 
-})
-
-app.get('/products/:id', (req, res) => {
+app.get('/products/:id', async (req, res) => {
 
     const id = +req.params.id;
 
-    const main = async () => {
-        const products = await manager.getProducts();
+    const products = await manager.getProducts();
 
-        const validProd = products.find(product => product.id === id);
+    const validProd = products.find(product => product.id === id);
 
-        if(!validProd) {
-            res.send({"error": "Product doesn't exist"});
-        }
-
-        res.send(validProd)
-        
+    if (!validProd) {
+        res.send({ "error": "Product doesn't exist" });
     }
 
-    main();
+    res.send(validProd)
 })
 
 app.listen(8080, () => {
