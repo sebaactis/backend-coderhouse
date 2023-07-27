ae2f9c97aeb459316027c8d3e74c496e37387745
Sebastian Actis <Sebastian.Actis@globalprocessing.com.ar>
2023-07-27 15:40:34 -0300

    cart documentation 50%

--- src/presentation/routes/cartsRouter.js
+++ src/presentation/routes/cartsRouter.js
@@ -8,8 +8,8 @@ cartsRouter.post('/', create)
 cartsRouter.post('/:cid/product/:pid', addToCart)
 cartsRouter.post('/purchase/:cid', purchase)
 cartsRouter.put('/:cid', updateCart)
-cartsRouter.put('/:cid/products/:pid', updateOneProductCart)
-cartsRouter.delete('/:cid/products/:pid', removeFromCart)
+cartsRouter.put('/:cid/product/:pid', updateOneProductCart)
+cartsRouter.delete('/:cid/product/:pid', removeFromCart)
 cartsRouter.delete('/:cid', removeAllCart)
 
 export default cartsRouter;
